5cc0a4d15275dd9f08c4af9726df6c3a98076c5e
Lucas Morato <luucasmorato@gmail.com>
2021-08-08 16:44:47 -0300

    create project and basic config

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import accountsRouter from "./routes/accounts.js";
+import { promises as fs, read } from "fs";
+
+const { readFile, writeFile } = fs;
+
+const app = express();
+app.use(express.json());
+app.use("/account", accountsRouter);
+
+app.listen(3333, async () => {
+  try {
+    await readFile("accounts.json");
+  } catch (err) {
+    const initialJson = {
+      nextId: 1,
+      accounts: [],
+    };
+
+    writeFile("accounts.json", JSON.stringify(initialJson))
+      .then(() => console.log("json file created"))
+      .catch((err) => console.log(`error: ${err}`));
+  }
+});
