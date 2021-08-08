a2acf33052d85b7d46fa21a1a1c8f6fe22669628
Lucas Morato <luucasmorato@gmail.com>
2021-08-08 17:19:55 -0300

    created post method

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,5 +1,25 @@
 import express from "express";
+import { promises as fs } from "fs";
 
 const router = express.Router();
+const { readFile, writeFile } = fs;
+
+router.post("/", async (req, res) => {
+  try {
+    let newAccount = req.body;
+    const data = JSON.parse(await readFile("accounts.json"));
+
+    newAccount = {
+      id: data.nextId++,
+      ...newAccount,
+    };
+    data.accounts.push(newAccount);
+
+    await writeFile("accounts.json", JSON.stringify(data));
+    res.send(newAccount);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
 
 export default router;
