097b90475888d9a0b3aaa724caf804d7d8fed450
Lucas Morato <luucasmorato@gmail.com>
2021-08-23 21:54:10 -0300

    add basic auth

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import express from "express";
-import accountsRouter from "./src/routes/account.routes.js";
+import accountsRouter from "./routes/account.routes.js";
 import { promises as fs } from "fs";
 import winston from "winston";
 import cors from "cors";
+import basicAuth from "express-basic-auth";
 
 global.fileName = "accounts.json";
 
@@ -22,11 +23,44 @@ global.logger = winston.createLogger({
 
 const { readFile, writeFile } = fs;
 
+function getRole(username) {
+  if (username == "admin") {
+    return "admin";
+  } else if (username == "lucas") {
+    return "role";
+  }
+}
+
+function authorize(...allowed) {
+  const isAllowed = (role) => allowed.indexOf(role) > -1;
+
+  return (req, res, next) => {
+    if (req.auth.user) {
+      const role = getRole(req.auth.user);
+      if (isAllowed(role)) {
+        next();
+      } else {
+        res.status(401).send("Role not allowed");
+      }
+    } else {
+      res.status(403).send("User not found");
+    }
+  };
+}
+
 const app = express();
 app.use(express.json());
-app.use("/account", accountsRouter);
-
 app.use(cors());
+app.use(
+  basicAuth({
+    authorizer: (username, password) => {
+      const userMatches = basicAuth.safeCompare(username, "admin");
+      const passwordMatches = basicAuth.safeCompare(password, "admin");
+      return userMatches && passwordMatches;
+    },
+  })
+);
+app.use("/account", authorize("admin", "role"), accountsRouter);
 
 app.listen(3333, async () => {
   try {
